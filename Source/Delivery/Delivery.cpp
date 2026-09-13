4765115d0566c6a95cc326853fd42b6b1df9e2f9
caihongqiu417 <2634933302@qq.com>
2026-09-13 20:58:40 +0800

    导入新人物

--- Source/Delivery/Delivery.cpp
+++ Source/Delivery/Delivery.cpp
@@ -3,6 +3,6 @@
 #include "Delivery.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Delivery, "Delivery" );
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, Delivery, "Delivery");
 
 DEFINE_LOG_CATEGORY(LogDelivery)
